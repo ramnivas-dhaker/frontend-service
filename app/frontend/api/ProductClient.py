0c7752744083cd0dd6b65de8c87552b406cc4ecc
Ramnivas Dhaker <ramnivasdhaker@Ramnivass-MacBook-Pro-2.local>
2023-04-25 22:13:54 +0530

    update connections

--- app/frontend/api/ProductClient.py
+++ app/frontend/api/ProductClient.py
@@ -1,12 +1,15 @@
 import requests
 
-host_url = 'http://0.0.0.0:5558'
+# host_url = 'http://0.0.0.0:5558'
+host_url = 'http://product:5558'
 
 class ProductClient:
 
     @staticmethod
     def get_product(slug):
         response = requests.request(method="GET", url=f'{host_url}/api/product/' + slug)
+        print("--------------product----------------",response )
+
         product = response.json()
         return product
 
@@ -14,4 +17,6 @@ class ProductClient:
     def get_products():
         r = requests.get(f'{host_url}/api/products')
         products = r.json()
+        print("--------------products----------------",products )
+
         return products
